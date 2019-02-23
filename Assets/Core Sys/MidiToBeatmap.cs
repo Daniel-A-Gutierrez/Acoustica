0c49dc90ea55cca59982266466982b69ed9dfdb3
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2019-02-23 00:11:17 -0800

    First Midi to Beatmap Conversion

--- Assets/Core Sys/MidiToBeatmap.cs
+++ Assets/Core Sys/MidiToBeatmap.cs
@@ -10,25 +10,40 @@ using Melanchall.DryWetMidi.Devices;
 using Melanchall;
 
 using System.Linq;
-
+[RequireComponent(typeof (NoteOrigin))]
 public class MidiToBeatmap : MonoBehaviour
 {
     // Start is called before the first frame update
     void somemethod()
     {
         MidiFile mf = MidiFile.Read("Assets/Midis/e.mid");
-        TrackChunk tc = (TrackChunk)mf.GetTrackChunks().First();
-        using (var notesManager = new NotesManager(tc.Events))
+        var tempoMap = mf.GetTempoMap();
+        foreach(TrackChunk tc in mf.GetTrackChunks())
         {
-            NotesCollection notes = notesManager.Notes;
-            IEnumerator<Note> ien = notes.GetEnumerator();
-            foreach(Note n in notes)
+            using (var notesManager = new NotesManager(tc.Events))
             {
-                //n = ien.Current;
-                print(n.NoteNumber);
+                NotesCollection notes = notesManager.Notes;
+                IEnumerator<Note> ien = notes.GetEnumerator();
+                foreach(Note n in notes)
+                {
+                    MetricTimeSpan metricTime = TimeConverter.ConvertTo<MetricTimeSpan>(n.Time, tempoMap);
+                    GetComponent<NoteOrigin>().EnqueueNote( "TestNote"/* tc.ChunkId*/,n.NoteNumber, 
+                                metricTime.Milliseconds/1000f, tempoMap.Tempo.AtTime(n.Time).BeatsPerMinute , lifetime:4);
+                    /* for later 
+                    switch(tc.ChunkId)
+                    {
+                        case "TestNote":
+                            GetComponent<NoteOrigin>().EnqueueNote( "TestNote" '''tc.ChunkId''' ,n.NoteNumber, 
+                                metricTime.Milliseconds/1000f, tempoMap.Tempo.AtTime(n.Time).BeatsPerMinute , lifetime:4);
+                            break;
+                        default:
+                            Debug.Log("ChunkId not found");
+                            break;
+                    }
+                    */
+                }
             }
         }
-
     }
     void Start()
     {
--- Assets/Core Sys/NoteOrigin.cs
+++ Assets/Core Sys/NoteOrigin.cs
@@ -10,57 +10,63 @@ public class NoteOrigin : MonoBehaviour
     //each frame, check the time, and generate notes from the deque until their start time is past the current time. 
     //"automation notes" will contain a string corresponding to a function, and strings corresponding to args for those functions.
     Queue<GameObject> TestNotes;
+
+    Dictionary<string,Queue<GameObject>> channels; //the key for a note type and its channel should be identical.
+    Dictionary<string, GameObject> noteTypes;
     float lastSpawnTime;
-    void Start()
+
+    void Awake() //midi to beatmap happens at start and requires this so, it must be awake
     {
         TestNotes = new Queue<GameObject>();
-        GenerateBeatmap(TestNotes);
+        channels =  new Dictionary<string,Queue<GameObject>>(); //the key for a note type and its channel should be identical.
+        noteTypes = new Dictionary<string, GameObject>();
+        channels.Add("TestNote",TestNotes);
+        noteTypes.Add("TestNote", TestNote);
+        //GenerateBeatmap(TestNotes);
         lastSpawnTime = 0;
     }
-    //enqueues a note into testNotes
-    void EnqueueNote(int position, float spawnTime, float tempo, int lifetime) //potentially create an array of queues for note types, and index
+    //enqueues a note into testNotes 
+    //void func(params object[] stuff) {} -> func('a', "B" , 48.3, 2)
+    //void func(int a =0, int b=0, int c=0) {} ->  func(a: 3, c:2)
+    //this will need to take variable args at some point.
+    public void EnqueueNote(string channelName ,int position, float spawnTime, float tempo, int lifetime)
     {
         if(lastSpawnTime > spawnTime) { Debug.Log("Beatmap problem : Successive notes not chronologically sequential. " 
             + spawnTime + " , " + lastSpawnTime);}
-        GameObject go = Instantiate(TestNote,Vector3.zero,Quaternion.identity);
-        go.GetComponent<NoteParent>().Setup(position,spawnTime,tempo,lifetime);
-        go.SetActive(false);
-        TestNotes.Enqueue(go);
-    }
+        if(channels[channelName] == null) { Debug.Log("Channel Name Not found : " + channelName + "." ) ; }
+        if(noteTypes[channelName] == null) { Debug.Log("Channel Name Not found : " + channelName + "." ) ; }
 
-    void GenerateBeatmap(Queue<GameObject> Notes)
-    {
-        int tempo = 60;
-        int life = 4;
-        for(int i = 0; i < 8; i++)
+        GameObject go = Instantiate(noteTypes[channelName],Vector3.zero,Quaternion.identity);
+
+        switch(channelName)
         {
-            GameObject go = Instantiate(TestNote,Vector3.zero,Quaternion.identity);
-            GameObject go2 = Instantiate(TestNote,Vector3.zero,Quaternion.identity);
-            go.GetComponent<NoteParent>().Setup(0 ,i,tempo,life);
-            go2.GetComponent<NoteParent>().Setup(64,i,tempo,life);
-            go.SetActive(false);
-            go2.SetActive(false);
-            Notes.Enqueue(go);
-            Notes.Enqueue(go2);
+            case "TestNote":
+                go.GetComponent<NoteParent>().Setup(position,spawnTime,tempo,lifetime);
+                break;
+            default:
+                Debug.Log("Switch statement: case not found");
+                break;
         }
+        
+        go.SetActive(false);
+        channels[channelName].Enqueue(go);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(TestNotes.Count > 0)
+        foreach( Queue<GameObject> channel in channels.Values)
         {
-            while(TestNotes.Peek().GetComponent<NoteParent>().SpawnTime <= Time.time)
+            if(channel.Count > 0)
             {
-                TestNotes.Peek().SetActive(true);
-                TestNotes.Dequeue();
-                if(TestNotes.Count==0)
-                    break;
+                while(channel.Peek().GetComponent<NoteParent>().SpawnTime <= Time.time)
+                {
+                    channel.Peek().SetActive(true);
+                    channel.Dequeue();
+                    if(channel.Count==0)
+                        break;
+                }
             }
         }
-        /* if(Random.Range(0,1f)>.9f)
-        {
-            Instantiate(TestNote,Vector3.zero,Quaternion.identity);
-        }*/
     }
 }
